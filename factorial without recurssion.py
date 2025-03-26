88de0ee2cf0d94f36f9bfced027a9bd1474d8a66
Bhavuk <bhavuk0998.becse24@chitkara.edu.in>
2025-03-26 11:08:06 +0530

    added t&c with css

---
+++ factorial without recurssion.py
@@ -0,0 +1,47 @@
+# a= int(input("Enter the number: "))
+# if(a == 0):
+#     fact = 1
+# fact = 1
+# for i in range(1,a+1):
+#     fact = fact*i
+# print("Factorial of", a, "is", fact)
+
+
+# n1=int(input("ENTER REQUIRED LIMIT OF SERIES"))
+# def rec (n):
+#         a=0
+#         b=1
+#         N2=b
+#         C=1
+#         if n==1:
+#             print(a)
+#         elif n==2:
+#             print(a,b)
+#         else :
+#             print(a, end=" ")
+#             while C<=n-1:
+#                 print(N2, end=" ")
+#                 N2=a+b
+#                 a,b=b,N2
+#                 C+=1
+#         print()
+
+# rec(n1)
+
+# n=str(int(input("enter an integer:")))
+# a=0
+# for i in n:
+#      a =a+int(i)**len(n)
+# if int(n)==a:
+#      print(n,"is an amstrong number")
+# else:
+#      print(n,"is not an armstrong number")
+
+
+     
+
+
+
+
+
+
